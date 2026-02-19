6273ead6e0e49327175bf919cee11fb189a075ba
diego.anguiano <diego.anguiano@atmira.com>
2026-02-19 18:23:32 +0100

    Clases principal de la app

---
+++ src/main/java/com/example/codechange/CodeChangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codechange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeChangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codechange/CodeChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codechange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
